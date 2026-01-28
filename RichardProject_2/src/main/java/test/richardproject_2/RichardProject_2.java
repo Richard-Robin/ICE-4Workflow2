792e6f899ecf0f4dc2d6ceb9e9697358bb8a9033
Richard-Robin <robinsr@sheridancollege.ca>
2026-01-27 20:04:01 -0500

    implemented the divide method and tested it.

---
+++ RichardProject_2/src/main/java/test/richardproject_2/RichardProject_2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package test.richardproject_2;
+
+/**
+ *
+ * @author richa
+ */
+public class RichardProject_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        double result = divide(6,2);
+        System.out.println("Division Result: "+result);
+    }
+    
+    public static double divide(double a , double b){
+    
+        return a / b;
+    }
+}
